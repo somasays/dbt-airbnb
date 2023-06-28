c4976ed3684c7148b994b9eef15b03d0c422845a
Somasundaram Sekar <somasundaram@outlook.com>
2023-06-28 11:23:44 +0200

    add macros and packages

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -7,7 +7,10 @@
 WITH src_reviews as (
     SELECT * from {{ ref('src_reviews') }}
 )
-select * from src_reviews
+select
+    {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+    *
+from src_reviews
 where review_text is not null
 {% if is_incremental() %}
     AND review_date > (select max(review_date) from {{ this }})
